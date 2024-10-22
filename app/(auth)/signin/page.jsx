8ba2f69190baa7b2ec2147ba765abe1748b6a175
Antonio Kodheli <antoniokodheli@Antonios-MacBook-Pro.local>
2024-10-21 21:32:47 -0400

    new changes

--- app/(auth)/signin/page.jsx
+++ app/(auth)/signin/page.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Page(){
+    
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState("")
